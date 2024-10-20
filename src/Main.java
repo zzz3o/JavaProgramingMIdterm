c17022a04cd57ed3a6b83d48c44619eb93b839a3
zzz3o <zzzeow3@gmail.com>
2024-10-21 01:01:35 +0900

    계산기 기본 틀 작성

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+//import ui.CalculatorFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author zzz3o
+ *
+ */
+public class Main extends JFrame {
+    Main(){
+        setTitle("계산기");
+
+        setLayout(new BorderLayout());
+        text();
+        keypad();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(530, 250);
+        setVisible(true);
+    }
+    void text(){
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new BorderLayout());
+
+        JTextArea textArea = new JTextArea(3,40);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        textArea.setText(" 0");
+        textArea.setEditable(true);
+        textArea.setForeground(Color.LIGHT_GRAY);
+
+        textPanel.add(scrollPane, BorderLayout.CENTER);
+        add(textPanel, BorderLayout.NORTH);
+    }
+    void keypad() {
+        JPanel keypadPanel = new JPanel();
+        keypadPanel.setLayout(new GridLayout(5, 5, 3, 3));  // 5x5
+
+        String[] buttons = {
+                "Backspace ", " " , " ", "CE", "C", "7", "8", "9", "/", "sqrt", "4", "5", "6", "x", "%", "1", "2", "3", "-", "1/x", "0", "+/-", ".", "+", "="
+        };
+
+        int count;
+        // for문을 사용하여 버튼 생성 및 패널에 추가
+        for (int i = 0; i < buttons.length; i++) {
+            JButton button = new JButton(buttons[i]);
+            button.setBackground(Color.WHITE);
+
+            // 행의 4번째와 5번째 버튼을 빨간색으로 나머지를 파란색으로 설정
+            if (i % 5 == 3 || i % 5 == 4) {
+                button.setForeground(Color.GRAY);
+            } else {
+                button.setForeground(Color.BLACK);
+            }
+            keypadPanel.add(button);
         }
+
+        add(keypadPanel, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+
+        new Main();
+
+
     }
 }
